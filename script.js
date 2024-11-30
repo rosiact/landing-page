1e8d3a5f1adc17cb479da631225a0a017dafaa7a
rosiact <rosimeireac@hotmail.com>
2024-11-30 15:54:51 -0300

    incluindo imagens

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
+let index = 0;
+
+function moverSlide(step) {
+  const items = document.querySelectorAll(".items");
+  index = index + step;
+
+  if (index >= items.length) {
+    index = 0;
+  }
+
+  if (index < 0) {
+    index = items.length - 1;
+  }
+  const carousel = document.querySelector(".carousel");
+  carousel.style.transform = `translateX(-${index * 100}%)`;
+}
+
+setInterval(() => moverSlide(1), 3000);
+
 let slideArual = 0;
 const slides = document.getElementById("slides");
 const totalSlides = document.querySelectorAll(".slide").length;
 
 document.getElementById("proximo").addEventListener("click", () => {
-    slideArual = (slideArual + 1) % totalSlides;
-    updateslider();
+  slideArual = (slideArual + 1) % totalSlides;
+  updateslider();
 });
 
 document.getElementById("anterior").addEventListener("click", () => {
-    slideArual = (slideArual - 1 + totalSlides) % totalSlides;
-    updateslider();
+  slideArual = (slideArual - 1 + totalSlides) % totalSlides;
+  updateslider();
 });
 function updateslider() {
-    const offset = -slideArual * 600;
-    slides.style.transform =`translateX(${offset}px)`;
+  const offset = -slideArual * 600;
+  slides.style.transform = `translateX(${offset}px)`;
 }
-
-
-
-
